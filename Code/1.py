70de8fd062a9f95f58fbfe6eead7a37f7cfd4f14
Tejassveer Vasant <147965837+Tejassveer08@users.noreply.github.com>
2025-12-01 12:46:55 +0530

    Add position tracking logic in 1.py
    Implement position tracking based on directional commands from input file.

---
+++ Code/1.py
@@ -0,0 +1,25 @@
+from collections import defaultdict, Counter
+with open("D1input.txt", "r") as f:
+    D = f.read()
+
+pos = 50
+p1 = 0
+p2 = 0
+
+for line in D.splitlines():
+    d = line[0]
+    amt = int(line[1:])
+    for _ in range(amt):
+        if d == 'L':
+            pos = (pos - 1 + 100) % 100
+        else:
+            pos = (pos + 1) % 100
+
+        if pos == 0:
+            p2 += 1  
+
+    if pos == 0:
+        p1 += 1  
+
+print(p1)
+print(p2)
